7374aa82edf6224782376883225400e1f88a7b3c
Harisivams <harisivam203@gmail.com>
2024-06-10 20:51:51 +0530

    Clg project

---
+++ intro.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  print("Hi Dart and Fluuter");
+  print('''  hello
+  maths
+  and
+  biology''');
+ print(5-3);
+
+}
---
+++ variables.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  var x=6,v=2;
+  var Sivam='Hari';
+  var y=11.10;
+  print(x);
+  print('x = $x');
+  print('3-1= ${3-1}');
+  print(Sivam);
+  print(y-1);
+  print('y = $y');
+}
