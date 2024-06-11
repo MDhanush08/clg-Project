451f93c2b49acce420c77db01c6f6b0d8f85fb1b
Harisivams <harisivam203@gmail.com>
2024-06-11 22:24:27 +0530

    added

--- variables.dart
+++ variables.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
 void main()
 {
-  var x=6,v=2;
+ /* var x=6,v=2;
   var Sivam='Hari';
   var y=11.10;
   print(x);
@@ -8,5 +9,15 @@ void main()
   print('3-1= ${3-1}');
   print(Sivam);
   print(y-1);
-  print('y = $y');
+  print('y = $y'); */
+
+  //var n;
+  print("hii");
+  print("any number");
+ // var n= int.parse(stdin.readLineSync()!);
+  //print(n);
+ // for(n=n+1;n<=10;n++){
+  //  print(n);
+   // n++;
+ // }
 }
